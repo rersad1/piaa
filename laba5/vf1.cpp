7a253afc01c9365b9f06384ccbe85472d5edeaf5
rersad <dlyastem@yandex.ru>
2025-06-03 16:32:47 +0300

    aho сложность

--- laba5/vf1.cpp
+++ laba5/vf1.cpp
@@ -7,31 +7,36 @@ using namespace std;
 
 int main() {
     int replaceCost, insertCost, deleteCost;
+    // Ввод стоимости операций: замена, вставка, удаление
     cin >> replaceCost >> insertCost >> deleteCost;
     
     string A, B;
+    // Ввод строк A и B
     cin >> A >> B;
     
     int n = A.length();
     int m = B.length();
 
+    // Вывод исходных данных
     cout << "Строка A: " << A << endl;
     cout << "Строка B: " << B << endl;
     cout << "Стоимости: замена=" << replaceCost << ", вставка=" << insertCost << ", удаление=" << deleteCost << endl;
     cout << endl;
 
+    // Таблица для хранения расстояний редактирования
     vector<vector<int>> editDistance(n + 1, vector<int>(m + 1, 0));
 
-    // Инициализация первой строки
+    // Инициализация первой строки (вставка символов)
     for (int j = 1; j <= m; j++) {
         editDistance[0][j] = editDistance[0][j-1] + insertCost;
     }
 
-    // Инициализация первого столбца
+    // Инициализация первого столбца (удаление символов)
     for (int i = 1; i <= n; i++) {
         editDistance[i][0] = editDistance[i-1][0] + deleteCost;
     }
 
+    // Вывод начальной таблицы расстояний
     cout << "Начальная таблица расстояний:" << endl;
     cout << "    ";
     for (int j = 0; j <= m; j++) {
@@ -50,17 +55,21 @@ int main() {
     }
     cout << endl;
 
+    // Основной цикл динамического программирования
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             if (A[i-1] == B[j-1]) {
+                // Если символы совпадают, берем значение по диагонали (без затрат)
                 editDistance[i][j] = editDistance[i-1][j-1];
                 cout << "Символы '" << A[i-1] << "' совпадают, берем диагональ: " << editDistance[i][j] << endl;
             } 
             else {
+                // Вычисляем стоимости операций
                 int replace = editDistance[i-1][j-1] + replaceCost;
                 int insert = editDistance[i][j-1] + insertCost;
                 int delete_op = editDistance[i-1][j] + deleteCost;
                 
+                // Выбираем минимальную стоимость
                 editDistance[i][j] = min({replace, insert, delete_op});
                 
                 cout << "Символы '" << A[i-1] << "' и '" << B[j-1] << "' не совпадают:" << endl;
@@ -70,6 +79,7 @@ int main() {
         }
     }
     
+    // Вывод итоговой таблицы расстояний
     cout << endl << "Итоговая таблица расстояний:" << endl;
     cout << "    ";
     for (int j = 0; j <= m; j++) {
@@ -86,7 +96,7 @@ int main() {
         }
         cout << endl;
     }
-    
+
     cout << endl << "Минимальное расстояние редактирования: " << editDistance[n][m] << endl;
 
     return 0;
